6868429cf45bf4ae3c221fd1a60d06f5de14c786
Coral Cota Riera <ccota@intricom.es>
2024-01-15 12:48:35 +0100

    Empezamos laboratorio 8.2 array methods ts

--- src/main.ts
+++ src/main.ts
@@ -1,3 +1,45 @@
-import "./style.css";
+interface Reserva {
+    id: number;
+    precio: number;
+    habitacion: string;
+    prepago: boolean;
+    completadaConExito: boolean;
+}
 
-console.log("Hello Typescript!");
+const reservas: Reserva[] = [
+    {
+    id: 23453,
+    precio: 250,
+    habitacion: "standard",
+    prepago: false,
+    completadaConExito: true,
+    },
+    {
+    id: 56456,
+    precio: 150,
+    habitacion: "superior",
+    prepago: false,
+    completadaConExito: true,
+    },
+    {
+    id: 43243,
+    precio: 550,
+    habitacion: "standard",
+    prepago: true,
+    completadaConExito: false,
+    },
+    {
+    id: 23223,
+    precio: 550,
+    habitacion: "standard",
+    prepago: true,
+    completadaConExito: true,
+    },
+    {
+    id: 89232,
+    precio: 650,
+    habitacion: "superior",
+    prepago: true,
+    completadaConExito: false,
+    },
+];
